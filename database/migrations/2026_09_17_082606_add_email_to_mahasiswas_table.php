9a74c6664494e44871818217901412bd61f9f4b4
Lina Fitriana Rahmi <fitriana010525@domain.com>
2026-09-17 19:30:40 +0800

    PRAKTIKUM 4: MODEL & ELOQUENT ORM

--- database/migrations/2026_09_17_082606_add_email_to_mahasiswas_table.php
+++ database/migrations/2026_09_17_082606_add_email_to_mahasiswas_table.php
@@ -1,13 +1,22 @@
-public function up(): void
-{
-    Schema::table('mahasiswas', function (Blueprint $table) {
-        $table->string('email')->nullable()->after('nama');
-    });
-}
+<?php
 
-public function down(): void
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-    Schema::table('mahasiswas', function (Blueprint $table) {
-        $table->dropColumn('email');
-    });
-}
+    public function up(): void
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            $table->string('email')->nullable()->after('nama');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            $table->dropColumn('email');
+        });
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Mahasiswa;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/mahasiswa', function () {
+    $data = Mahasiswa::all();
+    return view('mahasiswa.index', compact('data'));
 });
