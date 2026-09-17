4acd77ae020652be0f50b5f2cb239ec069e4cc74
Lina Fitriana Rahmi <fitriana010525@domain.com>
2026-09-17 16:30:53 +0800

    PRAKTIKUM 3: MIGRATION — VERSION CONTROL UNTUK DATABASE

--- database/migrations/2026_09_17_065236_create_mahasiswas_table.php
+++ database/migrations/2026_09_17_065236_create_mahasiswas_table.php
@@ -10,12 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('mahasiswas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('mahasiswas', function (Blueprint $table) {
+        $table->id();
+        $table->string('nim', 20)->unique();
+        $table->string('nama');
+        $table->string('prodi');
+        $table->enum('semester', ['1', '2', '3', '4', '5', '6', '7', '8']);
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
